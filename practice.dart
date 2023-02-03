0c6ecb1b3a439cec03499d9543af9aa6944a54f5
Kaleab213 <kaleabtibebu400@gmail.com>
2023-02-03 19:09:45 +0300

    initial commit

---
+++ practice.dart
@@ -0,0 +1,8 @@
+main() {
+  for (int i = 99; i > 0; i--) {
+    int j = i - 1;
+    print('$i bottles of beer on the wall, $i bottles of beer.');
+    print('Take one down and pass it around, $j bottles of beer on the wall');
+    print('');
+  }
+}
